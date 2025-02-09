5ec811528461c66644b69ff5e66e13c147c470c3
Steven7zzz <q1824711081@gmail.com>
2025-02-08 18:27:19 -0600

    Make the table sortable by column

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,11 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    column = params[:column] || "title"
+    direction = params[:direction] || "asc" 
+
+    # SELECT * FROM movies ORDER BY #{} #{};
+    @movies = Movie.order("#{column} #{direction}")
   end
 
   # GET /movies/1 or /movies/1.json
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def sortable_column(column, label)
+        order = (params[:column] == column && params[:direction] == "asc") ? "desc" : "asc"
+        text = params[:column] == column ? (params[:direction] == "asc" ? "asc" : "desc") : "sort"
+
+        "#{label} ".html_safe + link_to(text, { column: column, direction: order })
+    end
 end
+  
